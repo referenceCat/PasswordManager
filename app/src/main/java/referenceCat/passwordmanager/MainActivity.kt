eb908e7c05b336159a16d42906c394f0f4e782a8
referenceCat <mi.buyanov@yandex.ru>
2024-03-14 17:31:24 +0300

    main activity clean-up

--- app/src/main/java/referenceCat/passwordmanager/MainActivity.kt
+++ app/src/main/java/referenceCat/passwordmanager/MainActivity.kt
@@ -2,7 +2,6 @@ package referenceCat.passwordmanager
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
@@ -23,7 +22,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -52,25 +50,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-                Application()
+            Application()
         }
     }
-
 }
 
-@Preview
 @Composable
-fun Application (navController: NavHostController = rememberNavController()) {
+fun Application(navController: NavHostController = rememberNavController()) {
     val editScreenInitContentViewModel = viewModel<EditScreenInitContentViewModel>()
     val backStackEntry by navController.currentBackStackEntryAsState()
     navController.enableOnBackPressed(false)
     val currentScreen = Screen.valueOf(
-        backStackEntry?.destination?.route ?: if (PasswordsStorage.getInstance().isMasterPasswordInitiated(LocalContext.current)) Screen.LoginRoute.name else Screen.RegistrationRoute.name,
+        backStackEntry?.destination?.route ?: if (PasswordsStorage.getInstance()
+                .isMasterPasswordInitiated(LocalContext.current)
+        ) Screen.LoginRoute.name else Screen.RegistrationRoute.name,
     )
 
     Scaffold(
         topBar = {
-            AppBar(
+            ApplicationTopBar(
                 canNavigateBack = navController.previousBackStackEntry != null,
                 navigateUp = { navController.navigateUp() },
                 currentScreen = currentScreen,
@@ -78,63 +76,17 @@ fun Application (navController: NavHostController = rememberNavController()) {
                 )
         }
     ) { innerPadding ->
-        Log.d(null, "Nav start")
-        NavHost(
+        ApplicationNavigation(
             modifier = Modifier.padding(innerPadding),
             navController = navController,
-            startDestination = if (!PasswordsStorage.getInstance().isMasterPasswordInitiated(LocalContext.current))
-                Screen.RegistrationRoute.name
-            else if (!PasswordsStorage.getInstance().isMasterPasswordApplied() )
-                Screen.LoginRoute.name
-                    else
-                Screen.ListRoute.name
-        ) {
-            composable(route = Screen.RegistrationRoute.name) {
-                RegistrationScreen(onSuccessfulRegistration = {navController.navigate(Screen.ListRoute.name)})
-            }
-
-            composable(route = Screen.LoginRoute.name) {
-                LoginScreen(onSuccessfulLogin = {navController.navigate(Screen.ListRoute.name)})
-            }
-
-            composable(route = Screen.ListRoute.name) {
-
-                // user cant get back to login or register screen
-                // TODO user need to log in back after this
-                val activity = (LocalContext.current as? Activity)
-                BackHandler(true) {
-                    activity?.finish()
-                }
-
-                ListScreen(
-                    onActionButtonClick = {
-                        editScreenInitContentViewModel.clearData()
-                        navController.navigate(Screen.EntryEditRoute.name)
-                                          },
-                    onEditClick = {
-                        id: Int, name: String, website: String, password: String ->
-                        editScreenInitContentViewModel.setData(id, name, website, password)
-                        navController.navigate(Screen.EntryEditRoute.name)
-                    }
-                )
-            }
-
-            composable(route = Screen.EntryEditRoute.name) {
-                EditScreen(id = editScreenInitContentViewModel.id.intValue,
-                    name = editScreenInitContentViewModel.name.value,
-                    website = editScreenInitContentViewModel.website.value,
-                    password = editScreenInitContentViewModel.password.value,
-                    onSave = {navController.navigate(Screen.ListRoute.name)})
-            }
-        }
-
+            viewModel = editScreenInitContentViewModel
+        )
     }
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar(
+fun ApplicationTopBar(
     modifier: Modifier = Modifier,
     currentScreen: Screen,
     canNavigateBack: Boolean,
@@ -158,5 +110,60 @@ fun AppBar(
                 }
             }
         )
+    }
+}
+
+
+@Composable
+fun ApplicationNavigation(
+    modifier: Modifier,
+    navController: NavHostController,
+    viewModel: EditScreenInitContentViewModel,
+) {
+    var startDestination = Screen.RegistrationRoute.name
+
+    if (PasswordsStorage.getInstance().isMasterPasswordInitiated(LocalContext.current))
+        startDestination = Screen.RegistrationRoute.name
+
+    if (PasswordsStorage.getInstance().isMasterPasswordApplied())
+        startDestination = Screen.ListRoute.name
+
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Screen.RegistrationRoute.name) {
+            RegistrationScreen(onSuccessfulRegistration = { navController.navigate(Screen.ListRoute.name) })
+        }
+
+        composable(route = Screen.LoginRoute.name) {
+            LoginScreen(onSuccessfulLogin = { navController.navigate(Screen.ListRoute.name) })
+        }
+
+        composable(route = Screen.ListRoute.name) {
+            val activity = (LocalContext.current as? Activity)
+            BackHandler(true) {
+                activity?.finish()
+            }
+            ListScreen(
+                onActionButtonClick = {
+                    viewModel.clearData()
+                    navController.navigate(Screen.EntryEditRoute.name)
+                },
+                onEditClick = { id: Int, name: String, website: String, password: String ->
+                    viewModel.setData(id, name, website, password)
+                    navController.navigate(Screen.EntryEditRoute.name)
+                }
+            )
+        }
+
+        composable(route = Screen.EntryEditRoute.name) {
+            EditScreen(id = viewModel.id.intValue,
+                name = viewModel.name.value,
+                website = viewModel.website.value,
+                password = viewModel.password.value,
+                onSave = { navController.navigate(Screen.ListRoute.name) })
+        }
     }
 }
